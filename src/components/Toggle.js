88b8743b6d5bd847f78c2d3c93486ff2b6c4e2ed
tyler kim <yoonsoo.kim11@gmail.com>
2023-12-12 18:30:13 -0500

    complete practice!!!

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+
+const[toggleOn, setToggleOn] = useState(false)
+//addevent, call back function, inside handletoggle, setstate with settottleon, update text
+function handleToggle(){
+  setToggleOn(!toggleOn)
+}
+
+  return (
+  <button style={{background: toggleOn? 'red':'white'}} onClick={handleToggle}>{toggleOn? 'On':'Off'}</button>
+  )
 }
 
 export default Toggle;
+
+
